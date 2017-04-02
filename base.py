16ab4bfe2c651e26b3282ac4e49cad38069ca915
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-02 12:17:46 -0400

    Updated from Brython Server: 4/2/2017 12:17:46 PM

--- base.py
+++ base.py
@@ -1,8 +1,9 @@
-E=input("Input the measure of the vertical angle (E):")
-A=input("Input the measure of the azimuth angle (A):")
-H=input("Input the height (Z):")
-Xl=
-Yl=
+E=input("Input the measure of the vertical angle (E): ")
+A=input("Input the measure of the azimuth angle (A): ")
+H=input("Input the height (Z): ")
+Xl=input("Input the x coordinate of the antenna (Xl): ")
+Yl=input("Input the y coordinate of the antenna (Yl): ")
+Zl=0
 B=61
 Rr=((Xl-())+(Yl-())+(Zl-(H-B)))**(1/2)
 print(Rr)
