c3b0a202c7fca63b75c7c354e7f34014f8de37f3
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-02 12:26:39 -0400

    Updated from Brython Server: 4/2/2017 12:26:38 PM

--- base.py
+++ base.py
@@ -6,5 +6,5 @@ Xl=input("Input the x coordinate of the antenna (Xl): ")
 Yl=input("Input the y coordinate of the antenna (Yl): ")
 Zl=0
 B=61
-Rr=((Xl-())+(Yl-())+(Zl-(H-B)))**(1/2)
+Rr=(((Xl-((H/(tan(90-E)))*(sin(90-A))))**2)+((Yl-((H/(tan(90-E)))*(sin(A))))**2)+((Zl-(H-B))**2))**(1/2)
 print(Rr)
