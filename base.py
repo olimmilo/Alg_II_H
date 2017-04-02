99b1735108f1f3a91958d2ff0ec693b70c7a2308
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-02 12:14:57 -0400

    Updated from Brython Server: 4/2/2017 12:14:57 PM

--- base.py
+++ base.py
@@ -1,4 +1,8 @@
 E=input("Input the measure of the vertical angle (E):")
 A=input("Input the measure of the azimuth angle (A):")
-Z=input("Input the height (Z):")
-Rr=4**(1/2)
+H=input("Input the height (Z):")
+Xl=
+Yl=
+B=61
+Rr=((Xl-())+(Yl-())+(Zl-(H-B)))**(1/2)
+print(Rr)
