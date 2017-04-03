03eb0f981aae34189a3f348c69cf0de6103b3d3e
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-03 00:35:42 -0400

    Updated from Brython Server: 4/3/2017 12:35:40 AM

--- base.py
+++ base.py
@@ -1,13 +1,13 @@
 import math
-E=int(input("Input the measure of the vertical angle (E): "))
-A=int(input("Input the measure of the azimuth angle (A): "))
-H=int(input("Input the height (Z): "))
-Xl=int(input("Input the x coordinate of the antenna (Xl): "))
-Yl=int(input("Input the y coordinate of the antenna (Yl): "))
+E=float(input("Input the measure of the vertical angle (E): "))
+A=float(input("Input the measure of the azimuth angle (A): "))
+H=float(input("Input the height (Z): "))
+Xl=float(input("Input the x coordinate of the antenna (Xl): "))
+Yl=float(input("Input the y coordinate of the antenna (Yl): "))
 Zl=0
 B=61
 Rr=(((Xl-((H/(math.tan(90-E)))*(math.sin(90-A))))**2)+((Yl-((H/(math.tan(90-E)))*(math.sin(A))))**2)+((Zl-(H-B))**2))**(1/2)
-AZIM=
+AZIM=2
 
-print("The range is equal to:",Rr)
-print("The azimuth is equal to:",AZIM)
+print("The range is equal to:",Rr,"meters")
+print("The azimuth is equal to:",AZIM,"\u00b0")
