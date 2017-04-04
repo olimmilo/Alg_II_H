609bb8f7eedf3436e2bef69087a107f2690dd2e4
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-04 09:10:37 -0400

    Updated from Brython Server: 4/4/2017 9:10:36 AM

--- base.py
+++ base.py
@@ -14,7 +14,7 @@ Xl=float(input("Input the x coordinate of the antenna (Xl): "))
 Yl=float(input("Input the y coordinate of the antenna (Yl): "))
 Zl=0
 B=61
-Rr=(((Xl-((H/(math.tan(m90-E)))*(math.sin(90-A))))**2)+((Yl-((H/(math.tan(90-E)))*(math.sin(A))))**2)+((Zl-(H-B))**2))**(1/2)
+Rr=(((Xl-((H/(math.tan(deg2rad(90-E))))*(math.sin(deg2rad(90-A)))))**2)+((Yl-((H/(math.tan(deg2rad(90-E))))*(math.sin(deg2rad(A)))))**2)+((Zl-(H-B))**2))**(1/2)
 AZIM=2
 
 print("The range is equal to:",Rr,"meters")
