1c47aa1b1a0e560cc0a80616896fe412f83fd23c
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-04 09:09:00 -0400

    Updated from Brython Server: 4/4/2017 9:09:00 AM

--- base.py
+++ base.py
@@ -1,4 +1,12 @@
 import math
+def rad2deg(radians):
+	pi = math.pi
+	degrees = 180 * radians / pi
+	return degrees
+def deg2rad(degrees):
+	pi = math.pi
+	radians = pi * degrees / 180
+	return radians
 E=float(input("Input the measure of the vertical angle (E): "))
 A=float(input("Input the measure of the azimuth angle (A): "))
 H=float(input("Input the height (Z): "))
@@ -6,7 +14,7 @@ Xl=float(input("Input the x coordinate of the antenna (Xl): "))
 Yl=float(input("Input the y coordinate of the antenna (Yl): "))
 Zl=0
 B=61
-Rr=(((Xl-((H/(math.tan(90-E)))*(math.sin(90-A))))**2)+((Yl-((H/(math.tan(90-E)))*(math.sin(A))))**2)+((Zl-(H-B))**2))**(1/2)
+Rr=(((Xl-((H/(math.tan(m90-E)))*(math.sin(90-A))))**2)+((Yl-((H/(math.tan(90-E)))*(math.sin(A))))**2)+((Zl-(H-B))**2))**(1/2)
 AZIM=2
 
 print("The range is equal to:",Rr,"meters")
