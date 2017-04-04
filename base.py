aece90a47ca0bc56980d621e190a16e5603a9eea
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-04 09:27:24 -0400

    Updated from Brython Server: 4/4/2017 9:27:24 AM

--- base.py
+++ base.py
@@ -15,8 +15,21 @@ Yl=float(input("Input the y coordinate of the antenna (Yl): "))
 Zl=0
 B=61
 Rr=(((Xl-((H/(math.tan(deg2rad(90-E))))*(math.sin(deg2rad(90-A)))))**2)+((Yl-((H/(math.tan(deg2rad(90-E))))*(math.sin(deg2rad(A)))))**2)+((Zl-(H-B))**2))**(1/2)
-ELEV=90-rad2deg(asin((H-61)/Rr))
-if 
+ELEV=90-rad2deg(math.asin((H-61)/Rr))
+Xp=((H/(math.tan(deg2rad(90-E))))*(math.sin(deg2rad(90-A))))
+Yp=((H/(math.tan(deg2rad(90-E))))*(math.sin(deg2rad(A))))
+if Xp and Yp > 0:
+    QUAD=1
+    AZIM=math.atan(rad2deg((Yl-Yp)/(Xl-Xp)))
+elif Xp > 0 and Yp < 0:
+    QUAD=4
+    AZIM=(math.atan(rad2deg((Yl-Yp)/(Xl-Xp))))+360
+elif Xp < 0 and Yp < 0:
+    QUAD=3
+    AZIM=(math.atan(rad2deg((Yl-Yp)/(Xl-Xp))))-180
+elif Xp < 0 and Yp > 0:
+    QUAD=2
+    AZIM=(math.atan(rad2deg((Yl-Yp)/(Xl-Xp))))+180
 
 print("The measure of the vertical angle of the beacon relative to the antenna:",ELEV,"\u00b0")
 print("The azimuth of the beacon relative to the antenna:",AZIM,"\u00b0")
