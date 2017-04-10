2b26fec94530641fc292e3971154645c95c4f13b
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-10 11:59:38 -0400

    Update work.php

--- work.php
+++ work.php
@@ -8,7 +8,7 @@
         $Xl=40.6;
         $Yl=-106;
         $Zl=0;
-        $B=deg2rad(61);
+        $B=tan(61);
         /* $Rr=((($Xl-(($H/(tan(deg2rad(90-$E))))*(sin(deg2rad(90-$A)))))**2)+(($Yl-(($H/(tan(deg2rad(90-$E))))*(sin(deg2rad($A)))))**2)+(($Zl-($H-$B))**2))**(1/2);
         $ELEV=90-rad2deg(asin(($H-61)/$Rr));
         $Xp=(($H/(tan(deg2rad(90-$E))))*(sin(deg2rad(90-$A))));
