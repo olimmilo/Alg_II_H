28c8978cac40a82beb8fc7458f2b3f77768342fd
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-10 08:55:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<form action="welcome.php" method="post">
+<form action="work.php" method="post">
 Vertical Angle (E): <input type="text" name="E"><br>
 Azimuth angle (A): <input type="text" name="A"><br>
 Height (Z): <input type="text" name="H"><br>
