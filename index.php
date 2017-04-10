b483d67d5dab75afe08c97972bcfb556116b8dab
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-10 08:46:47 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Vertical Angle (E): <input type="text" name="E"><br>
+Azimuth angle (A): <input type="text" name="A"><br>
+Height (Z): <input type="text" name="H"><br>
+<input type="submit">
+</form>
+
+
+
+</body>
+</html>
