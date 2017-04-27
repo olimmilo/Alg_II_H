9a254b40984e0cdda2cee5850fdadfd654bfb713
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-04-27 09:34:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 Vertical Angle (E): <input type="text" name="E"><br>
 Azimuth angle (A): <input type="text" name="A"><br>
 Height (Z): <input type="text" name="H"><br>
-X coordinate of the anntenna (X<sub>L</sub>): <input type="text" name="XL"><br>
-Y coordinate of the anntenna (Y<sub>L</sub>): <input type="text" name="YL"><br>
+X coordinate of the anntenna (X<sub>L</sub>): <input type="text" name="X"><br>
+Y coordinate of the anntenna (Y<sub>L</sub>): <input type="text" name="Y"><br>
 <input type="submit">
 </form>
 
